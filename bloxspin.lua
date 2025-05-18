2c12dba5b485d5b980aa83e3e9e5b5582ed67c18
Zepth <taitanerenwearenumber@gmail.com>
2025-05-18 16:52:27 +0700

    Update bloxspin.lua

--- bloxspin.lua
+++ bloxspin.lua
@@ -1,7 +1,7 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
-   Name = "custom bloxspin",
+   Name = "custom bloxspin 0.2",
    Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
    LoadingTitle = "bloxspin",
    LoadingSubtitle = "by zepthical",
